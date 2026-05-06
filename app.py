cca2c9bd3cb1e7585ae810b48e2e01e75824f8df
Aravind AK <akaravind110@gmail.com>
2026-05-06 12:52:56 +0530

    added reverse integer solution

---
+++ app.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        if x < 0:
+            sign = -1
+        else:
+            sign = 1
+            
+        x = abs(x)
+
+        rev = 0
+        while x != 0:
+            digit = x % 10
+            x = x // 10
+            if rev > 214748364:
+                return 0
+            rev = rev * 10 + digit
+        rev = rev * sign
+
+        return rev
